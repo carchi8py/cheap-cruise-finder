463795dbda2455f6300bc4871f2d0dffd5b1e41f
Chris Archibald <carchi8py@gmail.com>
2017-10-08 12:05:47 -0700

    Adding Cruiseline to the database

--- src/cruise.py
+++ src/cruise.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
+from sqlalchemy import create_engine, exists
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, CruiseLine, Ship, Port, Curise
+
+engine = create_engine('sqlite:///db.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 #To start with i'll hard code 21 (with is hawaiian cruises).
 DESTINATION = "21"
 PARAMS = {"destination": DESTINATION}
@@ -11,6 +20,18 @@ def main():
     # the first cruise row is the header of the table, which we don't care about so we will skip them
     for cruise in cruises[1:]:
         cruise_data = get_cruise_data(cruise)
+        add_to_db(cruise_data)
+
+def add_to_db(cruise_data):
+    #Check to see if a curise line exists
+    if not session.query(exists().where(CruiseLine.name == cruise_data[1])).scalar():
+        add_cruiseline(cruise_data[1])
+        print("Adding %s to database" % cruise_data[1])
+
+def add_cruiseline(cruise_line):
+    new_cruiseline = CruiseLine(name = cruise_line)
+    session.add(new_cruiseline)
+    session.commit()
 
 def get_cruise_data(cruise):
     date = cruise.find("td", {"class": "table-date"}).text
