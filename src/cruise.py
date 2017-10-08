8303498c4348e1e5a6be7b47a7812c6160b00a08
Chris Archibald <carchi8py@gmail.com>
2017-10-08 11:49:35 -0700

    moving some code to different functions

--- src/cruise.py
+++ src/cruise.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import sys
 
 #To start with i'll hard code 21 (with is hawaiian cruises).
 DESTINATION = "21"
@@ -8,20 +7,26 @@ PARAMS = {"destination": DESTINATION}
 URL = "https://cruises.affordabletours.com/search/advanced_search"
 
 def main():
+    cruises = find_search_results()
+    # the first cruise row is the header of the table, which we don't care about so we will skip them
+    for cruise in cruises[1:]:
+        cruise_data = get_cruise_data(cruise)
+
+def get_cruise_data(cruise):
+    date = cruise.find("td", {"class": "table-date"}).text
+    line = cruise.find("td", {"class": "table-line"}).text
+    ship = cruise.find("td", {"class": "table-ship"}).text
+    destination = cruise.find("td", {"class": "table-destination"}).text
+    departs = cruise.find("td", {"class": "table-departs"}).text
+    nights = cruise.find("td", {"class": "table-nights"}).text
+    price = cruise.find("td", {"class": "table-price"}).text
+    return [date, line, ship, destination, departs, nights, price]
+
+def find_search_results():
     r = requests.get(URL, params=PARAMS)
     soup = BeautifulSoup(r.text, "html.parser")
     results = soup.find("table", {"class": "search-results"})
-    cruises = results.findAll("tr")
-    # the first cruise row is the header of the table, which we don't care about so we will skip them
-    for cruise in cruises[1:]:
-        date = cruise.find("td", {"class": "table-date"}).text
-        line = cruise.find("td", {"class": "table-line"}).text
-        ship = cruise.find("td", {"class": "table-ship"}).text
-        destination = cruise.find("td", {"class": "table-destination"}).text
-        departs = cruise.find("td", {"class": "table-departs"}).text
-        nights = cruise.find("td", {"class": "table-nights"}).text
-        price = cruise.find("td", {"class": "table-price"}).text
-        print(date, line, ship, destination, departs, nights, price)
+    return results.findAll("tr")
 
 if __name__ == "__main__":
     main()
