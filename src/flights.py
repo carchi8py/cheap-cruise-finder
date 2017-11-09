f409a377b4e0afd7a43985fe0ac8156c4301a6ba
Chris Archibald <carchi8py@gmail.com>
2017-11-08 16:24:49 -0800

    add an example of what a flight looks like to the documentation

--- src/flights.py
+++ src/flights.py
@@ -102,6 +102,106 @@ def generate_flight_url(date_obj, from_airport, to_airport):
     :param from_airport: The airport the flight is leaving from
     :param to_airport: The airport the flight is arriving at
     :return: The url that contain the flight information, or none
+
+    .. code-block:: json
+    
+    {
+        "mapIdfrom": "las-vegas",
+        "duration": {
+            "total": 5220,
+            "return": 0,
+            "departure": 5220
+        },
+        "flyTo": "SJC",
+        "conversion": {
+            "USD": 22,
+            "EUR": 19
+        },
+        "deep_link": "https://www.kiwi.com/deep?from=LAS&to=SJC&departure=29-11-2017&flightsId=332966413&price=19&passengers=1&affilid=picky&lang=en&currency=USD&booking_token=TjFsU9KOyjpEDqm5dNSrO5K0FG+q3rKETaHbA5WJ2yQXDnTgCWyBzCSbEggtTNtGqPQE9TDm1pkvplYzuWIbdLa3Ge63TzbPtiiWDE7WRL9ll50gaJwrQoQm3Qiny520+PbXZksFq3TMtBNSyBiWfQ/WJm0+hA7cnvs+mr6vpv7fkHQnZtNAdytvupuH0dEXy9YLI/g25CPYLN/4MUSWw5TQurbiO/Oo2L1VjG5JXVFhBqjM+TW4QS2rsBjG558+OkR6YJbeniEWnCamFvyloR/2z1lEyh+E9vPO3maOdZYhX/gElVWz8IFvETDe43KNq7lUWI07WS0VILHuFrvA7OBMkbamBGF6ST4siC/7zo+INq1m0cv1IIJYuqTfTI2UJnbmU5lmthSzsLsAqui/lkBY7B3X3L8iFk+Ih9a+dgNxYN/UZxbd2AYW02u3zjReO5oIhAFWfbhNF2SaVEwbWQIS1XE9bD59lOfcu2Xa8UB5Ggh7JIgRnaaptXHExQeNGLsOXxzFvP2vZJ4ES/JbsfdihyX5zNqb+K+hEqs1N8xz/oSN2EUiPiYYcLQdPq3mkc44u/YgSRK7/QxFT5Gbx7VM8xY4E2F+b5A6D3Z+LjA=",
+        "mapIdto": "san-francisco",
+        "nightsInDest": null,
+        "airlines": [
+        "F9"
+        ],
+        "id": "332966413",
+        "facilitated_booking_available": true,
+        "pnr_count": 1,
+        "fly_duration": "1h 27m",
+        "countryTo": {
+            "code": "US",
+            "name": "United States"
+        },
+        "baglimit": {
+            "hand_width": null,
+            "hand_length": null,
+            "hold_weight": 22,
+            "hand_height": null,
+            "hand_weight": null
+        },
+        "aTimeUTC": 1511989320,
+        "p3": 1,
+        "price": 22,
+        "type_flights": [
+        "lcc"
+        ],
+        "bags_price": {
+            "1": 28,
+            "2": 65
+        },
+        "cityTo": "San Jose",
+        "transfers": [],
+        "flyFrom": "LAS",
+        "dTimeUTC": 1511984100,
+        "p2": 1,
+        "countryFrom": {
+            "code": "US",
+            "name": "United States"
+        },
+        "p1": 1,
+        "dTime": 1511955300,
+        "found_on": [
+        "deprecated"
+        ],
+        "booking_token": "TjFsU9KOyjpEDqm5dNSrO5K0FG+q3rKETaHbA5WJ2yQXDnTgCWyBzCSbEggtTNtGqPQE9TDm1pkvplYzuWIbdLa3Ge63TzbPtiiWDE7WRL9ll50gaJwrQoQm3Qiny520+PbXZksFq3TMtBNSyBiWfQ/WJm0+hA7cnvs+mr6vpv7fkHQnZtNAdytvupuH0dEXy9YLI/g25CPYLN/4MUSWw5TQurbiO/Oo2L1VjG5JXVFhBqjM+TW4QS2rsBjG558+OkR6YJbeniEWnCamFvyloR/2z1lEyh+E9vPO3maOdZYhX/gElVWz8IFvETDe43KNq7lUWI07WS0VILHuFrvA7OBMkbamBGF6ST4siC/7zo+INq1m0cv1IIJYuqTfTI2UJnbmU5lmthSzsLsAqui/lkBY7B3X3L8iFk+Ih9a+dgNxYN/UZxbd2AYW02u3zjReO5oIhAFWfbhNF2SaVEwbWQIS1XE9bD59lOfcu2Xa8UB5Ggh7JIgRnaaptXHExQeNGLsOXxzFvP2vZJ4ES/JbsfdihyX5zNqb+K+hEqs1N8xz/oSN2EUiPiYYcLQdPq3mkc44u/YgSRK7/QxFT5Gbx7VM8xY4E2F+b5A6D3Z+LjA=",
+        "routes": [
+            [
+                "LAS",
+                "SJC"
+            ]
+        ],
+        "cityFrom": "Las Vegas",
+        "aTime": 1511960520,
+        "route": [
+            {
+                "bags_recheck_required": false,
+                "mapIdfrom": "las-vegas",
+                "flight_no": 1127,
+                "original_return": 0,
+                "lngFrom": -115.172789096832,
+                "flyTo": "SJC",
+                "guarantee": false,
+                "latTo": 37.3625984,
+                "source": "deprecated",
+                "combination_id": "332966413",
+                "id": "332966413",
+                "latFrom": 36.1146658358667,
+                "lngTo": -121.9290009,
+                "dTimeUTC": 1511984100,
+                "aTimeUTC": 1511989320,
+                "return": 0,
+                "price": 1,
+                "cityTo": "San Jose",
+                "flyFrom": "LAS",
+                "mapIdto": "san-francisco",
+                "dTime": 1511955300,
+                "found_on": "deprecated",
+                "airline": "F9",
+                "cityFrom": "Las Vegas",
+                "aTime": 1511960520
+            }
+        ],
+        "distance": 618.35
+    },
     """
     try:
         url = FLIGHT_URL1 + from_airport + FLIGHT_URL2 + to_airport + FLIGHT_URL3
